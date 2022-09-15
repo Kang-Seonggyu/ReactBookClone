d18b1f0acefdf3f2b925b900a199ab39c52bbbd0
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-15 16:35:35 +0900

    Ref 다루기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import ValidationSample from "./5_ref_namingDom/ValidationSample";
+import ValidationSample from "./5_ref_namingDom/1_ValidationSample";
+import ScrollBox from "./5_ref_namingDom/ScrollBox";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <ValidationSample/>
+        <ScrollBox />
       </header>
     </div>
   );
