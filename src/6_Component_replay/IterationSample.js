1f24b0a83b38605bf0e8aa41d81a008223b7f9ac
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-16 13:12:09 +0900

    목록 만드는 함수

---
+++ src/6_Component_replay/IterationSample.js
@@ -0,0 +1,16 @@
+const IterationSample = () => {
+    const names = ['눈사람','얼음','눈','바람'];
+    const nameList = names.map((name,index) => <li key={index}>{name}</li>);
+
+    return(
+        <ul>{nameList}</ul>
+        // <ul>
+        //     <li>눈사람</li>
+        //     <li>얼음</li>
+        //     <li>눈</li>
+        //     <li>바람</li>
+        // </ul>
+    );
+};
+
+export default IterationSample;
