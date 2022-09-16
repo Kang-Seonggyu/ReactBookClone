126a707d762bf50e295f11cbb7eedcbdf72f8407
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-16 14:49:44 +0900

    SASS 배우기.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import {Component} from "react";
-import ValidationSample from "./5_ref_namingDom/1_ValidationSample";
-import ScrollBox from "./5_ref_namingDom/ScrollBox";
+// import { Component } from "react";
+// import Info_plus_UseInputs from "./8_Hooks/Info_plus_UseInputs";
+//
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Info_plus_UseInputs />
+//             </div>
+//         );
+//     }
+//
+// }
+//
+// export default App;
+//
+import { Component } from "react";
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
     render() {
-        return (
-        <div className="App">
-          <header className="App-header">
-            <ValidationSample/>
-            <ScrollBox ref={(ref) => this.scrollBox=ref} />
-              <button onClick={() => this.scrollBox.scrollToBottom()}>
-                  맨 밑으로
-              </button>
-          </header>
-        </div>
-      );
+        return(
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        This is Test Page. Welcome!
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://github.com/Kang-Seonggyu"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Go to git : Seong-gyu
+                    </a>
+                </header>
+            </div>
+        );
     }
 }
 
