e50fd20f8a776016c04a17c32d29c2838d83f2e5
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-19 15:36:58 +0900

    12장 index

---
+++ src/12_immer_keep_immutlble/index_for12.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App_for12 from "./12_immer_keep_immutlble/App_for12";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div>
+        <App_for12/>
+    </div>
+
+);
