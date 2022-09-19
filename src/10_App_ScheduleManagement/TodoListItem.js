6698e021b819769a747ac8d9a3bdfbc3cce7043b
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-19 15:16:14 +0900

    10장과 11장 분리

--- src/10_App_ScheduleManagement/TodoListItem.js
+++ src/10_App_ScheduleManagement/TodoListItem.js
@@ -6,20 +6,21 @@ import {
 import cn from 'classnames';
 import './TodoListItem.css'
 
-const TodoListItem = ({todo}) => {
-    const { text, checked } = todo;
+const TodoListItem = ({todo, onRemove, onToggle}) => {
+    const { id, text, checked } = todo;
 
     return (
         <div className="TodoListItem">
-            <div className={cn('checkbox',{ checked })}>
+            <div className={cn('checkbox',{ checked })} onClick={() => onToggle(id)}>
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank /> }
                 <div className="text">{text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline />
             </div>
         </div>
     );
 };
 
+// 11장에서 React.memo 추가
 export default TodoListItem;
