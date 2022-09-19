f84cca2a358c3d52a4ba267c0be6a2a48e031770
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-19 16:58:30 +0900

    레이아웃을 이용하기

--- src/13_ReactRouter_DevSpa/App_for13.js
+++ src/13_ReactRouter_DevSpa/App_for13.js
@@ -2,13 +2,22 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profile from "./Profile";
+import Articles from "./Articles";
+import Article from "./Article";
+import Layout from "./Layout";
 
 const App_for13 = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home /> } />
-            <Route path="/about" element={<About /> } />
-            <Route path="/profiles/:username" element={<Profile /> } />
+            <Route element={<Layout /> }>
+                <Route index element={<Home /> } />
+                <Route path="/about" element={<About /> } />
+                <Route path="/profiles/:username" element={<Profile /> } />
+            </Route>
+            <Route path="/articles" element={<Articles /> }>
+                <Route path=":id" element={<Article /> } />
+            </Route>
+
         </Routes>
     );
 };
