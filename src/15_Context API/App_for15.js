e30c2604f3c20efe295bd5e5f3fae200ede10faf
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-20 11:07:05 +0900

    15장 Consumer 이용하기

---
+++ src/15_Context API/App_for15.js
@@ -0,0 +1,11 @@
+import ColorBox from "./Component/ColorBox";
+
+const App_for15 = () => {
+    return(
+        <div>
+            <ColorBox />
+        </div>
+    );
+};
+
+export default App_for15;
