bb7746a46d46d16b481bf7fe7993cdefdecafde8
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-20 10:37:25 +0900

    14장 뉴스 페이지 만들기

--- src/14_NewsViewer_UsingAPI/App_for14.js
+++ src/14_NewsViewer_UsingAPI/App_for14.js
@@ -1,25 +1,61 @@
-import {useState} from "react";
-import axios from "axios";
+//// API 테스트
+// import {useState} from "react";
+// import axios from "axios";
+//
+// // newsAPI 의 api key = 8f1e2a787f9342f591ce32f3c0050ade
+// const App_for14 = () => {
+//     const [data, setData] = useState(null);
+//     const onClick = async () => {
+//         try {
+//             const response = await axios.get(
+//                 'https://newsapi.org/v2/top-headlines?country=kr&apiKey=8f1e2a787f9342f591ce32f3c0050ade'
+//             );
+//             setData(response.data);
+//         } catch (e) {
+//             console.log(e);
+//         };
+//     };
+//     return (
+//         <div>
+//             <div>
+//                 <button onClick={onClick}>불러오기</button>
+//             </div>
+//             {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} /> }
+//         </div>
+//     );
+// };
+//
+// export default App_for14;
+
+// // News 웹 페이지 Route 없음.
+// import {useState, useCallback} from "react";
+// import NewsList from "./components/NewsList";
+// import Categories from "./components/Categories";
+//
+// const App_for14 = () => {
+//     const [category, setCategory] = useState('all');
+//     const onSelect = useCallback(category => setCategory(category), [])
+//     return (
+//         <>
+//             <Categories category={category} onSelect={onSelect} />
+//             <NewsList category={category} />
+//         </>
+//     );
+// };
+//
+// export default App_for14;
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
 
 const App_for14 = () => {
-    const [data, setData] = useState(null);
-    const onClick = async () => {
-        try {
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/todos/1'
-            );
-            setData(response.data);
-        } catch (e) {
-            console.log(e);
-        };
-    };
     return (
-        <div>
-            <div>
-                <button onClick={onClick}>불러오기</button>
-            </div>
-            {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} /> }
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<NewsPage />} />
+                <Route path="/:category" element={<NewsPage />} />
+            </Routes>
+        </Router>
     );
 };
 
