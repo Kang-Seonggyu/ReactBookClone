60bdd2a3c768ce4c135ae690a5c51b79ebd3393b
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-20 16:20:43 +0900

    17장 스토어만들기

---
+++ src/17_Redux_ReactAppStateManagement/index_for17.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createStore } from "redux";
+import rootReducer from "./Modules";
+
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
