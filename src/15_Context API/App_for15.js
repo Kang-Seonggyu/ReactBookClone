095d0dbc9566923036a17e493a06e0165d71dc71
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-20 11:41:53 +0900

    15장 마우스 클릭시 색변경

--- src/15_Context API/App_for15.js
+++ src/15_Context API/App_for15.js
@@ -1,10 +1,12 @@
 import ColorBox from "./Component/ColorBox";
 import {ColorProvider} from "./Context/color";
+import SelectColors from "./Component/SelectColors";
 
 const App_for15 = () => {
     return(
         <ColorProvider>
             <div>
+                <SelectColors />
                 <ColorBox />
             </div>
         </ColorProvider>
