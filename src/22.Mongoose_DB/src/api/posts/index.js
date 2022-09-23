0f48bacae4e7a9995a18a3fa187673bb3fa8f079
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-23 11:05:54 +0900

    22장 리팩토링 오류 수정

--- src/22.Mongoose_DB/src/api/posts/index.js
+++ src/22.Mongoose_DB/src/api/posts/index.js
@@ -5,11 +5,15 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
+// posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+// posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+// posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
 
+// // 리팩토링
 const post = new Router();
-post.get('/:id', postsCtrl.read);
-post.delete('/:id', postsCtrl.remove);
-post.patch('/:id', postsCtrl.update);
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes() );
 
