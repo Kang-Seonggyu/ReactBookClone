8a22ce2b5304ae3c16e626ff2c1f879c4e0f24c8
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-23 16:15:45 +0900

    23장 회원가입 구현

--- src/23.Json_Web_Token/src/models/user.js
+++ src/23.Json_Web_Token/src/models/user.js
@@ -1,9 +1,31 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypyt from 'bcrypt';
 
 const UserSchema = new Schema({
     username : String,
     hashedPassword : String,
 });
+
+UserSchema.methods.setPassword = async function(passwrod) {
+    const hash = await bcrypyt.hash(passwrod, 10);
+    this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function(password) {
+    const result = await bcrypyt.compare(password, this.hashedPassword);
+    return result;
+};
+
+UserSchema.methods.serialize = function() {
+    const data = this.toJSON();
+    delete data.hashedPassword;
+    return data;
+};
+
+UserSchema.statics.findByUsername = function(username) {
+    return this.findOne({ username });
+};
+
 
 const User = mongoose.model('User', UserSchema);
 export default User;
