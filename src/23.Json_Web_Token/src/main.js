75b4eb6be4dee3b737f76fc22c01333662605c4c
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-23 17:32:35 +0900

    23장 토큰 검증하기

--- src/23.Json_Web_Token/src/main.js
+++ src/23.Json_Web_Token/src/main.js
@@ -5,6 +5,7 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
@@ -13,7 +14,7 @@ const { PORT, MONGO_URI } = process.env;
 mongoose
 .connect(MONGO_URI)
     .then(() => {
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB Success');
     })
     .catch (e => {
         console.error(e);
@@ -28,6 +29,7 @@ router.use('/api', api.routes());
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 // app 인스턴스 전에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
