b3469444cb8c92c73c834c03e5bb74727a45acb3
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-26 09:21:57 +0900

    23장 로그인 했을 때만 API 사용권한부여

--- src/23.Json_Web_Token/src/api/posts/index.js
+++ src/23.Json_Web_Token/src/api/posts/index.js
@@ -1,10 +1,11 @@
 import Router from 'koa-router';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn,postsCtrl.write);
 // posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
 // posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
 // posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
@@ -12,8 +13,8 @@ posts.post('/', postsCtrl.write);
 // // 리팩토링
 const post = new Router();
 post.get('/', postsCtrl.read);
-post.delete('/', postsCtrl.remove);
-post.patch('/', postsCtrl.update);
+post.delete('/',checkLoggedIn, postsCtrl.remove);
+post.patch('/',checkLoggedIn, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes() );
 
