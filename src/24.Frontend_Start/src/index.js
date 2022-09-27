978a97cd6cf27e9909a37597942403e35bbd8c2f
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-27 11:19:18 +0900

    24장 중요 ! 모듈 위치 이상해서 계속터지던 오류 해결

--- src/24.Frontend_Start/src/index.js
+++ src/24.Frontend_Start/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from '../../../node_modules/react-router-dom/dist/index';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
