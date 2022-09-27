dd024421f7d5afa2288a2929e92fb4a757cbc3e9
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-27 17:20:30 +0900

    25장 에디터 컨테이너 만들기

--- src/24.Frontend_Start/src/pages/WritePage.js
+++ src/24.Frontend_Start/src/pages/WritePage.js
@@ -1,4 +1,4 @@
-import Editor from "../components/write/Editor";
+import EditorContainer from "../containers/write/EditorContainer";
 import TagBox from "../components/write/TagBox";
 import WriteActionButtons from "../components/write/WriteActionButtons";
 import Responsive from "../components/common/Responsive";
@@ -6,7 +6,7 @@ import Responsive from "../components/common/Responsive";
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
+            <EditorContainer />
             <TagBox />
             <WriteActionButtons />
         </Responsive>
