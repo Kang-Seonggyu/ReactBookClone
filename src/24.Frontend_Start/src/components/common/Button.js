33a941599f022ba118b8f109575310c35f77dd55
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-27 11:46:50 +0900

    24장 헤더 컴포넌트 생성

--- src/24.Frontend_Start/src/components/common/Button.js
+++ src/24.Frontend_Start/src/components/common/Button.js
@@ -1,7 +1,8 @@
 import styled, {css} from 'styled-components';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
     border : none;
     border-radius : 4px;
     font-size : 1rem;
@@ -14,7 +15,7 @@ const StyledButton = styled.button`
     &:hover {
         background : ${palette.gray[6]};
     }
-    
+
     ${props => 
         props.fullwidth &&
         css`
@@ -30,9 +31,23 @@ const StyledButton = styled.button`
             &:hover {
                 background : ${palette.cyan[4]};
             }
-        `}  
+        `} 
 `;
 
-const Button = props => <StyledButton {...props} />;
+const StyledButton = styled.button`
+    ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+    ${buttonStyle}
+`;
+
+const Button = props => {
+    return props.to ? (
+        <StyledLink {...props} cyan={props.cyan ? 1 : 0 }/>
+    ) : (
+        <StyledButton {...props} />
+    );
+};
 
 export default Button;
