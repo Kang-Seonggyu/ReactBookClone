4843c027f5b7618fe07d2c44c078ddfb9574b09e
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-28 10:34:56 +0900

    26장 PostViewer UI 준비

---
+++ src/26_Frontend_Post_Search/src/pages/PostPage.js
@@ -0,0 +1,13 @@
+import HeaderContainer from "../containers/common/HeaderContainer";
+import PostViewer from "../components/post/PostViewer";
+
+const PostPage = () => {
+    return (
+        <>
+            <HeaderContainer />
+            <PostViewer />
+        </>
+    );
+};
+
+export default PostPage;
