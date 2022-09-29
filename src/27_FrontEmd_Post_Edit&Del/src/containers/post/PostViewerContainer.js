3d4b4e87c9c11df3cc846c82003f75fdb0a5aab9
Kang-Seonggyu <jokekim609@gmail.com>
2022-09-29 11:49:46 +0900

    27장 포스트 수정

--- src/27_FrontEmd_Post_Edit&Del/src/containers/post/PostViewerContainer.js
+++ src/27_FrontEmd_Post_Edit&Del/src/containers/post/PostViewerContainer.js
@@ -2,20 +2,25 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { readPost, unloadPost } from "../../modules/post";
 import PostViewer from "../../components/post/PostViewer";
-import { useNavigate, useParams } from "react-router-dom";
 import PostActionButton from "../../components/post/PostActionButtons";
+import { setOriginalPost } from "../../modules/write";
+import { removePost } from "../../lib/api/posts"
+import { useNavigate, useParams } from "react-router-dom";
 
 
 
 const PostViewerContainer = () => {
     // 처음 마운트될 때 포스트 읽기 API 요청
     // let params = useParams();
-    const {postId} = useParams(); // @뭐/여기값
+    const {postId} = useParams(); // 주소값/@ID/Post_ID
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-    const { post, error, loading } = useSelector(({ post, loading }) => ({
+    const { post, error, loading, user } = useSelector(
+        ({ post, loading, user }) => ({
         post : post.post,
         error : post.error,
         loading : loading['post/READ_POST'],
+        user : user.user,
     }));
 
     useEffect(() => {
@@ -26,12 +31,23 @@ const PostViewerContainer = () => {
         };
     }, [dispatch, postId]);
 
-    return <PostViewer 
-        post={post} 
-        loading={loading} 
-        error={error}
-        actionButtons={<PostActionButton />}    
-    />;
+    const onEdit = () => {
+        dispatch(setOriginalPost(post));
+        navigate('/write');
+    }
+
+    const ownPost = (user && user._id) === (post && post.user._id);
+
+    return  (
+        <PostViewer 
+            post={post} 
+            loading={loading} 
+            error={error}
+            actionButtons={
+                ownPost && <PostActionButton onEdit={onEdit} />
+            }    
+        />
+    );
 };
 
 export default PostViewerContainer;
