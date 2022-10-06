f81d603dc15a1bdb27651157d6804deac6e4c706
Kang-Seonggyu <jokekim609@gmail.com>
2022-10-06 15:02:41 +0900

    Moment를 이용한 캘린더 만들어보기

--- src/index.js
+++ src/index.js
@@ -11,9 +11,13 @@
 import App_for18 from "./18_Redux_Middleware_Asynchronous/App_for18";
 import ReactDOM from "react-dom/client";
 import ReactCalendar from "./test/CalendarAPI/ReactCalendar";
+import MomentCalendar from "./test/CalendarAPI/MomentCalendar";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ReactCalendar />
+    <>
+        <ReactCalendar />
+        <MomentCalendar />
+    </>
 );
